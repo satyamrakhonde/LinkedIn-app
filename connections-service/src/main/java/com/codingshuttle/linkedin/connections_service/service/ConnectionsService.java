5f017aa6e01fa4edaac876c07690c351c56894b4
satyamrakhonde <satyamrakhonde@gmail.com>
2025-09-06 12:22:27 +0530

    connection service

--- connections-service/src/main/java/com/codingshuttle/linkedin/connections_service/service/ConnectionsService.java
+++ connections-service/src/main/java/com/codingshuttle/linkedin/connections_service/service/ConnectionsService.java
@@ -1,4 +1,23 @@
 package com.codingshuttle.linkedin.connections_service.service;
 
+import com.codingshuttle.linkedin.connections_service.entity.Person;
+import com.codingshuttle.linkedin.connections_service.repository.PersonRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
 public class ConnectionsService {
+
+    private final PersonRepository personRepository;
+
+    public List<Person> getFirstDegreeConnections(Long userId) {
+        log.info("Getting first degree connections for uer with id: {}", userId);
+
+        return personRepository.getFirstDegreeConnections(userId);
+    }
 }
